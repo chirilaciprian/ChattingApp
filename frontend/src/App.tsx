4a752048ff814dbbdf024174d635a4d0d9420a78
chirilaciprian <ciprian.chirila001@gmail.com>
2026-02-04 14:19:53 +0200

    toast container added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ChatPage from './pages/Chat';
@@ -7,6 +8,7 @@ import './App.css';
 function App() {
   return (
     <Router>
+      <ToastContainer />
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
